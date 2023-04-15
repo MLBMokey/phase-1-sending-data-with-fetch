b46d292ecb0a3456cf986f446956a30164a767cf
Anthony Lopez <anthonydlr66@gmail.com>
2023-04-15 14:39:02 -0400

    creating new documents

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+const body = document.querySelector("body")
+
+function submitData(name, email){
+    const newUser = {
+        name: name,
+        email:email
+    }
+    
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+    },
+    body: JSON.stringify(newUser)
+})
+
+.then(res => res.json())
+
+.then(function(userObj) {
+    const div = document.createElement("div")
+    div.textContent = userObj.id
+    body.appendChild(div)
+})
+
+.catch(function() {
+    const li = document.createElement("li")
+    li.textContent = "Unauthorized Access"
+    body.appendChild(li)
+})}
+
